abe0b0a43fe94a3356ccc68a6210964e08809e71
Nicole Villa <nicolevilla.109@gmx.com>
2019-08-18 19:09:53 -0500

    Update scripts.js 1.2
    Created an array of objects for the first 6 pokemon. Their properties include name, height, & types.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,16 @@
-alert('Hello world');
+// this will be the repository of pokemon displayed in application
+let repository = [
+	{ name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] },
+	{ name: 'Ivysaur', height: 1, types: ['grass', 'poison'] },
+	{ name: 'Venusaur', height: 0.7, types: ['grass', 'poison'] },
+	{ name: 'Charmander', height: 0.6, types: ['fire'] },
+	{ name: 'Charmeleon', height: 1.1, types: ['fire'] },
+	{ name: 'Charizard', height: 1.7, types: ['fire'] }
+];
 
-var favoriteFood = 'Mexican Food';
-document.write(favoriteFood);
+console.log(repository[0]);
+console.log(repository[1]);
+console.log(repository[2]);
+console.log(repository[3]);
+console.log(repository[4]);
+console.log(repository[5]);
