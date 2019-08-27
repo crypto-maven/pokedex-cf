38d1b88751b188bdc46eac440cdc5e1d3de56a78
Nicole Villa <nicolevilla.109@gmx.com>
2019-08-26 21:14:43 -0500

    HTML, Scripts & Styling
    had the screen print the pokemons, alot of index.html styling in bulma, pokemon imgs.

--- js/scripts.js
+++ js/scripts.js
@@ -13,15 +13,12 @@ let pokemonRepo = [
 // }
 
 // for loop that iterates over each item in the repository
-// for (var printData = 0; printData < pokemonRepo.length; printData++) {
-// 	document.write(pokemonRepo[printData].name, pokemonRepo[printData].height);
-// }
-
-message = '';
 for (var printData = 0; printData < pokemonRepo.length; printData++) {
-	if () {
-		
-	} else (pokemonRepo[printData].height >= 1.5) {
+	document.write(pokemonRepo[printData].name, pokemonRepo[printData].height);
+}
+
+for (var printData = 0; printData < pokemonRepo.length; printData++) {
+	if (pokemonRepo[printData].height >= 1.5) {
 		document.write(
 			pokemonRepo[printData].name,
 			pokemonRepo[printData].height,
@@ -29,3 +26,26 @@ for (var printData = 0; printData < pokemonRepo.length; printData++) {
 		);
 	}
 }
+
+// functionality for the nav bar- from bulma documentation
+document.addEventListener('DOMContentLoaded', function() {
+	// get all "navbar burger" elements
+	var $navbarBurgers = Array.prototype.slice.call(
+		document.querySelectorAll('.navbar-burger'),
+		0
+	);
+	// check if there are any navbar burgers
+	if ($navbarBurgers.length > 0) {
+		// add a click event on each of them
+		$navbarBurgers.forEach(function($el) {
+			$el.addEventListener('click', function() {
+				// get the target from the "data-target" attribute
+				var target = $el.dataset.target;
+				var $target = document.getElementById(target);
+				// toggle the class on both the "navbar-burger" and the "navbar-menu"
+				$el.classList.toggle('is-active');
+				$target.classList.toggle('is-active');
+			});
+		});
+	}
+});
