fbc34fdaa9b3e68007289a4ebbfe4ddbd51bf197
Nicole Villa <nicolevilla.109@gmx.com>
2019-09-17 15:43:02 -0500

    Update to scripts.js
    Working on the pokemonRepo IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,24 @@
 // this will be the repository of pokemon displayed in application
-let pokemonRepo = [
-	{ name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] },
-	{ name: 'Ivysaur', height: 1, types: ['grass', 'poison'] },
-	{ name: 'Venusaur', height: 0.7, types: ['grass', 'poison'] },
-	{ name: 'Charmander', height: 0.6, types: ['fire'] },
-	{ name: 'Charmeleon', height: 1.1, types: ['fire'] },
-	{ name: 'Charizard', height: 1.7, types: ['fire'] }
-];
+// let pokemonRepo = [
+// 	{ name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] },
+// 	{ name: 'Ivysaur', height: 1, types: ['grass', 'poison'] },
+// 	{ name: 'Venusaur', height: 0.7, types: ['grass', 'poison'] },
+// 	{ name: 'Charmander', height: 0.6, types: ['fire'] },
+// 	{ name: 'Charmeleon', height: 1.1, types: ['fire'] },
+// 	{ name: 'Charizard', height: 1.7, types: ['fire'] }
+// ];
+
+// wrapping pokemonRepo in an IIFE
+(function(name, height) {
+	var pokemonRepository = [
+		{ name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] },
+		{ name: 'Ivysaur', height: 1, types: ['grass', 'poison'] },
+		{ name: 'Venusaur', height: 0.7, types: ['grass', 'poison'] },
+		{ name: 'Charmander', height: 0.6, types: ['fire'] },
+		{ name: 'Charmeleon', height: 1.1, types: ['fire'] },
+		{ name: 'Charizard', height: 1.7, types: ['fire'] }
+	];
+})(pokemonRepository);
 
 // for loop that iterates over each item in the repository
 for (var printData = 0; printData < pokemonRepo.length; printData++) {
@@ -23,7 +35,7 @@ for (var printData = 0; printData < pokemonRepo.length; printData++) {
 	}
 }
 
-// forEach that prints the details of the pokemon repo
+// forEach that prints the details of the pokemon repo exercise 1.4
 Object.keys(pokemonRepo).forEach(function(property) {
 	console.log(pokemonRepo[property]);
 });
