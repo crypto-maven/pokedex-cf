85cc2a60fe844d88868706d0b3503790d085f0ad
Nicole Villa <nicolevilla.109@gmx.com>
2019-09-17 16:29:23 -0500

    IIFE for pokemonRepo
    update IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -9,17 +9,22 @@
 // ];
 
 // wrapping pokemonRepo in an IIFE
-(function(pokemonRepository) {
-	var pokemonRepository = repository;
-	var repository = [
-		{ name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] },
-		{ name: 'Ivysaur', height: 1, types: ['grass', 'poison'] },
-		{ name: 'Venusaur', height: 0.7, types: ['grass', 'poison'] },
-		{ name: 'Charmander', height: 0.6, types: ['fire'] },
-		{ name: 'Charmeleon', height: 1.1, types: ['fire'] },
-		{ name: 'Charizard', height: 1.7, types: ['fire'] }
-	];
-})(pokemonRepository);
+var pokemonRepository = (function() {
+	var repository = [];
+	function add(pokemon) {
+		repository.push(pokemon);
+	}
+	function getAll() {
+		return repository;
+	}
+	return {
+		add: add,
+		getAll: getAll
+	};
+})();
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({ name: 'Pikachu' });
+console.log(pokemonRepository.getAll());
 
 // for loop that iterates over each item in the repository
 for (var printData = 0; printData < pokemonRepo.length; printData++) {
