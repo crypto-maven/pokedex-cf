c938775309d82cc969ed9278a2cd21b4c221402e
Nicole Villa <nicolevilla.109@gmx.com>
2019-11-08 17:30:21 -0600

    update scripts.js
    removed objects from the repository

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,6 @@
 // wrapping pokemonRepo in an IIFE
 var pokemonRepository = (function() {
-	var repository = [
-		{ name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] },
-		{ name: 'Ivysaur', height: 1, types: ['grass', 'poison'] },
-		{ name: 'Venusaur', height: 0.7, types: ['grass', 'poison'] },
-		{ name: 'Charmander', height: 0.6, types: ['fire'] },
-		{ name: 'Charmeleon', height: 1.1, types: ['fire'] },
-		{ name: 'Charizard', height: 1.7, types: ['fire'] }
-	];
+	var repository = [];
 	// the validation of the pokemon
 	function add(pokemon) {
 		if (
